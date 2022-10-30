a983f69545bad1d14dd4772c6f5069c85227a384
Daniel Costa <danielcc.jp@gmail.com>
2022-10-29 23:49:34 -0300

    feat: add rand number generator and parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    // Rust defaults to i32, a 32-bit number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess");
 
@@ -13,5 +20,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}")
+    // Shadowing
+    // Trim eliminates \n and \r, also whitespaces
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
